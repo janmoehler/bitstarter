e5060de30e498ea8d5ef1216757d653b081b6294
janmoehler <jan@moehler.se>
2013-07-10 07:52:56 +0000

    web.js updated

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+#!/usr/bin/env node
+var fs = require('fs');
 var express = require('express');
-
+var infile = "index.html";
+var readedbuffer = fs.readFileSync(infile);
+var readedstring = readedbuffer.toString();
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(readedstring);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
